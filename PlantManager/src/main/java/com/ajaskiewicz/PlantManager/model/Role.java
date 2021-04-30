5a2307bc914ffd463c77658d1b13a4c93a799192
AJaśkiewicz <agnieszka.jaskiewicz@gmail.com>
2021-04-30 15:31:38 +0200

    Sign in, sign up and logout flows

---
+++ PlantManager/src/main/java/com/ajaskiewicz/PlantManager/model/Role.java
@@ -0,0 +1,25 @@
+package com.ajaskiewicz.PlantManager.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
