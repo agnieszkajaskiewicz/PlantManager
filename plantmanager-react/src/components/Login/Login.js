077a8323a6429c910b667c2b28f171601bad2135
luke <lukjab@yahoo.pl>
2021-10-31 12:42:25 +0100

    add very initial version of login form, using React component with state

--- plantmanager-react/src/components/Login/Login.js
+++ plantmanager-react/src/components/Login/Login.js
@@ -2,11 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Login.module.css';
 
-const Login = () => (
-  <div className={styles.Login} data-testid="Login">
-    Login Component
-  </div>
-);
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {username: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('Podano następujący username: ' + this.state.username);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className={styles.Login} data-testid="Login">
+                Login Component
+                <div>
+                    <input id="tab-1" type="radio" name="tab" checked/> {/*todo dziwne nazwy, 'tab'*/}
+                    <label htmlFor="tab-1" className={styles.tab}>Sign In</label>
+                    <input id="tab-2" type="radio" name="tab" />
+                    <label htmlFor="tab-2" className={styles.tab}>Sign Up</label>
+                </div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="username" className={styles.label}>Username</label>
+                        <input type="text" className={styles.input} value={this.state.username} onChange={this.handleChange}/>
+                    </div>
+                    <input type="submit" value="Wyślij" />
+                </form>
+            </div>
+        )
+    }
+}
 
 Login.propTypes = {};
 
