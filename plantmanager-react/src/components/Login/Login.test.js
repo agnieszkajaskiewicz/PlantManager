851ecca11aa6c42d90236ec0734ab9fcc9261388
luke <lukjab@yahoo.pl>
2021-11-03 15:35:07 +0100

    Add first test to application

--- plantmanager-react/src/components/Login/Login.test.js
+++ plantmanager-react/src/components/Login/Login.test.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
 import Login from './Login';
 
 describe('<Login />', () => {
-  test('it should mount', () => {
-    render(<Login />);
-    
-    const login = screen.getByTestId('Login');
+    test('it should mount', () => {
+        render(<Login/>);
 
-    expect(login).toBeInTheDocument();
-  });
+        const login = screen.getByTestId('Login');
+
+        expect(login).toBeInTheDocument();
+    });
+
+    test('it should open \'sing up\' panel on click', () => {
+            //given
+            render(<Login/>);
+            const signUpLabel = screen.getByText('Sign Up');
+            //when
+            signUpLabel.click();
+            //then
+            const signUpButton = screen.getByRole('button');
+            expect(signUpButton.textContent).toEqual('Sign Up');
+        }
+    );
 });
