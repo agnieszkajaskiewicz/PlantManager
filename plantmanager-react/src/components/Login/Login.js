861c1b9abf96fb1225af38eea73aecbb09bf1911
luke <lukjab@yahoo.pl>
2021-11-03 15:16:01 +0100

    small cleanup

--- plantmanager-react/src/components/Login/Login.js
+++ plantmanager-react/src/components/Login/Login.js
@@ -51,12 +51,14 @@ class Login extends React.Component {
             <Form.Control id="password" type="password" value={this.state.password} className={styles.loginInput}
                           onChange={this.handleChange}/>
             <Form.Label htmlFor="repeatPassword" className={styles.loginInput}>Repeat password</Form.Label>
-            <Form.Control id="repeatPassword" type="password" value={this.state.repeatPassword} className={styles.loginInput}
+            <Form.Control id="repeatPassword" type="password" value={this.state.repeatPassword}
+                          className={styles.loginInput}
                           onChange={this.handleChange}/>
             <Form.Label htmlFor="email" className={styles.loginInput}>Email address</Form.Label>
             <Form.Control id="email" type="email" value={this.state.email} className={styles.loginInput}
                           onChange={this.handleChange}/>
-            <Button type="submit" className={styles.loginInput}>Sign Up</Button></>;
+            <Button type="submit" className={styles.loginInput}>Sign Up</Button>
+        </>;
 
         const signInForm = <>
             <Form.Label htmlFor="username" className={styles.loginInput}>Username</Form.Label>
@@ -65,9 +67,10 @@ class Login extends React.Component {
             <Form.Label htmlFor="password" className={styles.loginInput}>Password</Form.Label>
             <Form.Control id="password" type="password" value={this.state.username} className={styles.loginInput}
                           onChange={this.handleChange}/>
-            <Form.Check id="keepSignedIn" >
-                <Form.Check.Input type="checkbox" defaultChecked={true} />
-                <Form.Label htmlFor="keepSignedIn" className={styles.loginInput} style={{display: "inline-block", marginTop: "1.2em"}}>Keep me signed in</Form.Label>
+            <Form.Check id="keepSignedIn">
+                <Form.Check.Input type="checkbox" defaultChecked={true}/>
+                <Form.Label className={styles.loginInput} style={{display: "inline-block", marginTop: "1.2em"}}>Keep me
+                    signed in</Form.Label>
             </Form.Check>
             <Button type="submit" className={styles.loginInput} style={{marginTop: '15%'}}>Sign In</Button>
         </>
@@ -77,12 +80,14 @@ class Login extends React.Component {
                 <div>
                     <Form onSubmit={this.handleSubmit}>
                         <div className={styles.loginForm}>
-                            <Form.Check hidden={true} style={{display: 'none'}} inline id="choice-1" name="choice" type="radio"
+                            <Form.Check hidden={true} style={{display: 'none'}} inline id="choice-1" name="choice"
+                                        type="radio"
                                         defaultChecked={true} onChange={this.handleSelect}/>
                             <Form.Label htmlFor="choice-1"
                                         style={this.state.whichSelected === 'choice-1' ? selectedBorderStyle : unselectedBorderStyle}
                                         className={styles.choice}>Sign In</Form.Label>
-                            <Form.Check hidden={true} inline id="choice-2" name="choice" type="radio" onChange={this.handleSelect}/>
+                            <Form.Check hidden={true} inline id="choice-2" name="choice" type="radio"
+                                        onChange={this.handleSelect}/>
                             <Form.Label htmlFor="choice-2"
                                         style={this.state.whichSelected === 'choice-2' ? selectedBorderStyle : unselectedBorderStyle}
                                         className={styles.choice}>Sign Up</Form.Label>
