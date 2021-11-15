525b7ea6be7fe718b796d0c110791a3eb7bc4fdf
luke <lukjab@yahoo.pl>
2021-11-15 08:28:51 +0100

    small cleanup after css refactor

--- plantmanager-react/src/components/AppHeader/AppHeader.test.js
+++ plantmanager-react/src/components/AppHeader/AppHeader.test.js
@@ -4,7 +4,14 @@ import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
 import AppHeader from './AppHeader';
 
-describe('<App Header />', () => {
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate
+}));
+
+describe('<AppHeader />', () => {
     test('it should mount', () => {
         render(<AppHeader/>);
 
--- plantmanager-react/src/components/Homepage/Homepage.test.js
+++ plantmanager-react/src/components/Homepage/Homepage.test.js
@@ -4,6 +4,13 @@ import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
 import Homepage from './Homepage';
 
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate
+}));
+
 describe('<Homepage />', () => {
     test('it should mount', () => {
         render(<Homepage/>);
