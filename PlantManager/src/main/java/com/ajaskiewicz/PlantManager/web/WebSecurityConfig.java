3dd2a7d1943e6bddda1ee64dc28f273d7ae5016d
Łukasz <lukjab@yahoo.pl>
2021-11-18 18:50:47 +0100

    work in progress

--- PlantManager/src/main/java/com/ajaskiewicz/PlantManager/web/WebSecurityConfig.java
+++ PlantManager/src/main/java/com/ajaskiewicz/PlantManager/web/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -49,6 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/sign-in")
+                .successHandler((req, res, auth) -> res.setStatus(HttpStatus.OK.value()))
                 //.defaultSuccessUrl("/dashboard", true)
                 .permitAll()
                 .and()
