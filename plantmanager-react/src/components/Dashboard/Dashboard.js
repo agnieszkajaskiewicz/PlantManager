f462d0428acfd8893c5c73bf1c2ea3c80a062418
luke <lukjab@yahoo.pl>
2021-11-23 20:09:26 +0100

    add beautiful collapsable element to dashboard

--- plantmanager-react/src/components/Dashboard/Dashboard.js
+++ plantmanager-react/src/components/Dashboard/Dashboard.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import styles from './Dashboard.module.css';
+import {Collapse} from "react-bootstrap";
 
-const Dashboard = () => (
+const Dashboard = () => {
+    const [open, setOpen] = useState(true);
+    return (
+
+
   <div className={styles.Dashboard} data-testid="Dashboard">
     Dashboard Component
       Można użyć "card" z Boostrapa
@@ -10,11 +15,18 @@ const Dashboard = () => (
           <span>See plants that should be watered in next 3 days</span>
       </div>
       <br/>
-      <div className={styles.toBeWatered}> {/*this should be collapsible*/}
-          <span>Your plants:</span>
+      <div className={styles.toBeWatered} data-toggle="collapse"  data-target="#plants">
+          <input id="collapseCheck" className={styles.toggle} hidden={true} type="checkbox" defaultChecked={false}/>
+          <label htmlFor="collapseCheck" className={styles.lblToggle} onClick={() => setOpen(!open)}>Your plants:</label>
       </div>
+      <Collapse in={open}>
+          <div className={styles.container} id="plants">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim
+              keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+      </Collapse>
   </div>
-);
+)};
 
 Dashboard.propTypes = {};
 
