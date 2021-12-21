8f7f763caa830e574002abc61768ce8273c25e0d
Łukasz <lukjab@yahoo.pl>
2021-12-21 17:50:22 +0100

    update test so they separate concerns

--- PlantManager/src/test/java/com/ajaskiewicz/PlantManager/service/PlantServiceImplTest.java
+++ PlantManager/src/test/java/com/ajaskiewicz/PlantManager/service/PlantServiceImplTest.java
@@ -5,13 +5,11 @@ import com.ajaskiewicz.PlantManager.model.WateringSchedule;
 import com.ajaskiewicz.PlantManager.repository.PlantRepository;
 import com.ajaskiewicz.PlantManager.repository.UserRepository;
 import com.ajaskiewicz.PlantManager.repository.WateringScheduleRepository;
-import org.h2.table.Plan;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +20,7 @@ class PlantServiceImplTest {
 
     private static final Integer USER_ID = 1;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final int DEFAULT_INTERVAL = 3;
 
     private PlantService plantService;
 
@@ -39,41 +38,71 @@ class PlantServiceImplTest {
     }
 
     @Test
-    public void shouldCalculateCorrectDifferenceInDays() {
+    public void shouldCalculateCorrectNegativeDifferenceInDays() {
         //given
-        var allUserPlants = preparePlants();
-        when(plantRepository.findAllByUserId(USER_ID)).thenReturn(allUserPlants);
+        var plantToBeWateredTwoDaysAgo = preparePlantThatShouldBeWatered(1, -2);
+        when(plantRepository.findAllByUserId(USER_ID)).thenReturn(newArrayList(plantToBeWateredTwoDaysAgo));
+        //when
+        var result = plantService.findPlantsToBeWateredSoon(USER_ID);
+        //then
+        assertEquals(1, result.size());
+        assertEquals(-2, result.get(0).getWateringDifferenceInDays());
+    }
+
+    @Test
+    public void shouldCalculateCorrectPositiveDifferenceInDays() {
+        //given
+        var plantToBeWateredTomorrow = preparePlantThatShouldBeWatered(1, 1);
+        when(plantRepository.findAllByUserId(USER_ID)).thenReturn(newArrayList(plantToBeWateredTomorrow));
+        //when
+        var result = plantService.findPlantsToBeWateredSoon(USER_ID);
+        //then
+        assertEquals(1, result.size());
+        assertEquals(1, result.get(0).getWateringDifferenceInDays());
+    }
+
+    @Test
+    public void shouldSortPlantFromTheOldestWateredToTheMostRecentlyWatered() {
+        //given
+        var plantToBeWateredTomorrow = preparePlantThatShouldBeWatered(1, 1);
+        var plantToBeWateredTwoDaysAgo = preparePlantThatShouldBeWatered(2, -2);
+        when(plantRepository.findAllByUserId(USER_ID)).thenReturn(newArrayList(plantToBeWateredTomorrow, plantToBeWateredTwoDaysAgo));
         //when
         var result = plantService.findPlantsToBeWateredSoon(USER_ID);
         //then
         assertEquals(2, result.size());
-        assertEquals(-2, result.get(0).getWateringDifferenceInDays());
-        assertEquals(1, result.get(1).getWateringDifferenceInDays());
+        assertEquals(2, result.get(0).getId());
+        assertEquals(1, result.get(1).getId());
     }
 
-    private List<Plant> preparePlants() {
-        var plantThatShouldBeWateredTwoDaysAgo = new Plant();
-        plantThatShouldBeWateredTwoDaysAgo.setId(1);
-        var wateringScheduleThatShouldBeWateredTwoDaysAgo = new WateringSchedule();
-        wateringScheduleThatShouldBeWateredTwoDaysAgo.setLastWateredDate(LocalDate.now().minusDays(5).format(FORMATTER));
-        wateringScheduleThatShouldBeWateredTwoDaysAgo.setWateringInterval(3);
-        plantThatShouldBeWateredTwoDaysAgo.setWateringSchedule(wateringScheduleThatShouldBeWateredTwoDaysAgo);
-
-        var plantThatShouldBeWateredTomorrow = new Plant();
-        plantThatShouldBeWateredTomorrow.setId(2);
-        var wateringScheduleThatShouldBeWateredTomorrow = new WateringSchedule();
-        wateringScheduleThatShouldBeWateredTomorrow.setLastWateredDate(LocalDate.now().minusDays(2).format(FORMATTER));
-        wateringScheduleThatShouldBeWateredTomorrow.setWateringInterval(3);
-        plantThatShouldBeWateredTomorrow.setWateringSchedule(wateringScheduleThatShouldBeWateredTomorrow);
-
+    @Test
+    public void shouldFilterOutPlantThatShouldBeWateredInMoreThanThreeDaysInTheFuture() {
+        //given
+        var plantToBeWateredTomorrow = preparePlantThatShouldBeWatered(1, 1);
+        var plantToBeWateredTwoDaysAgo = preparePlantThatShouldBeWatered(2, -2);
         var plantThatShouldNotBeWatered = new Plant();
         plantThatShouldNotBeWatered.setId(3);
         var wateringScheduleThatShouldNotBeWatered = new WateringSchedule();
         wateringScheduleThatShouldNotBeWatered.setLastWateredDate(LocalDate.now().format(FORMATTER));
         wateringScheduleThatShouldNotBeWatered.setWateringInterval(4);
         plantThatShouldNotBeWatered.setWateringSchedule(wateringScheduleThatShouldNotBeWatered);
+        when(plantRepository.findAllByUserId(USER_ID)).thenReturn(newArrayList(plantThatShouldNotBeWatered, plantToBeWateredTomorrow, plantToBeWateredTwoDaysAgo));
+        //when
+        var result = plantService.findPlantsToBeWateredSoon(USER_ID);
+        //then
+        assertEquals(2, result.size());
+        assertEquals(2, result.get(0).getId());
+        assertEquals(1, result.get(1).getId());
+    }
 
+    private Plant preparePlantThatShouldBeWatered(int id, int wateringDifferenceInDays) {
+        var plantThatShouldBeWatered = new Plant();
+        plantThatShouldBeWatered.setId(id);
+        var wateringScheduleThatShouldBeWatered = new WateringSchedule();
+        wateringScheduleThatShouldBeWatered.setLastWateredDate(LocalDate.now().minusDays(-1 * wateringDifferenceInDays + DEFAULT_INTERVAL).format(FORMATTER));
+        wateringScheduleThatShouldBeWatered.setWateringInterval(3);
+        plantThatShouldBeWatered.setWateringSchedule(wateringScheduleThatShouldBeWatered);
 
-        return newArrayList(plantThatShouldBeWateredTwoDaysAgo, plantThatShouldBeWateredTomorrow, plantThatShouldNotBeWatered);
+        return plantThatShouldBeWatered;
     }
 }
