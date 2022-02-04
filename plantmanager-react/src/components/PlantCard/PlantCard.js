f4c130b2a327f66b883d3e496353d41ce6d4f007
Łukasz <lukjab@yahoo.pl>
2022-02-04 07:57:31 +0100

    connect delete plant operation to PlantCard component

--- plantmanager-react/src/components/PlantCard/PlantCard.js
+++ plantmanager-react/src/components/PlantCard/PlantCard.js
@@ -3,22 +3,32 @@ import styles from './PlantCard.module.css';
 import {Card} from "react-bootstrap";
 import addIcon from "../../img/addIcon.png";
 import bin from "../../img/bin.png";
-
+import {useDependencies} from '../../DependencyContext';
 //todo responsywny rozmiar czcionki
 //todo responsywna szerokość, wysokość przy nadmiernym skurczeniu
 
-const PlantCard = (props) => (
-    <Card className={styles.container} data-testid="PlantCard">
-        <img src={addIcon} alt="Add Plant" className={styles.plantImg}/>
-        <Card.Body className={styles.plantCard}>
-            <Card.Title>{props.plantData ? props.plantData.plantName : 'Your new plant'}</Card.Title>
-            <button className="appButton" style={ {width: 'auto', display: 'inline'} }>{props.plantData ? 'EDIT' : 'ADD'}</button>
-            {(props.plantData) &&
-                <img src={bin} alt="Remove plant" className={styles.removeImg}/>
-            }
-        </Card.Body>
-    </Card>
-);
+const PlantCard = (props) => {
+    const {plantService} = useDependencies();
+
+    const deletePlant = (plantId) => {
+        plantService.deletePlantById(plantId);
+    }
+
+    return (
+        <Card className={styles.container} data-testid="PlantCard">
+            <img src={addIcon} alt="Add Plant" className={styles.plantImg}/>
+            <Card.Body className={styles.plantCard}>
+                <Card.Title>{props.plantData ? props.plantData.plantName : 'Your new plant'}</Card.Title>
+                <button className="appButton"
+                        style={{width: 'auto', display: 'inline'}}>{props.plantData ? 'EDIT' : 'ADD'}</button>
+                {(props.plantData) &&
+                    <img src={bin} alt="Remove plant" className={styles.removeImg}
+                         onClick={() => deletePlant(props.plantData.id)} data-testid="trashbin"/>
+                }
+            </Card.Body>
+        </Card>
+    )
+};
 
 PlantCard.propTypes = {};
 
--- plantmanager-react/src/components/PlantCard/PlantCard.test.js
+++ plantmanager-react/src/components/PlantCard/PlantCard.test.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import PlantCard from './PlantCard';
+
+const mockedPlantService = {
+  deletePlantById: (plantId) => jest.fn()
+};
+
+jest.mock('../../DependencyContext', () => ({
+  useDependencies: () => ({
+    plantService: mockedPlantService
+  })
+}));
+
 
 describe('<PlantCard />', () => {
   test('it should mount', () => {
@@ -18,5 +29,20 @@ describe('<PlantCard />', () => {
 
   test('it should render add card when no data', () => {
 
+  });
+
+  test('it should delete plant on trash click', () => {
+    //given
+    const plantData = {
+      id: 1,
+      plantName: 'plantName'
+    }
+    render(<PlantCard plantData={plantData} />)
+    const removeButton = screen.getByTestId('trashbin');
+    jest.spyOn(mockedPlantService, 'deletePlantById');
+    //when
+    removeButton.click();
+    //then
+    expect(mockedPlantService.deletePlantById).toHaveBeenCalledWith(plantData.id);
   });
 });
