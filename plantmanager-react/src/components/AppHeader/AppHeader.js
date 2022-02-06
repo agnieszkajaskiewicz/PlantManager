20e272fdb5a9c0a2c5a06c73a7a36ee73e35b088
agnieszkajaskiewicz <agnieszka.jaskiewicz@gmail.com>
2022-02-06 13:39:23 +0100

    AppHeader - css stuff

--- plantmanager-react/src/components/AppHeader/AppHeader.js
+++ plantmanager-react/src/components/AppHeader/AppHeader.js
@@ -9,9 +9,32 @@ const AppHeader = () => {
     const navigate = useNavigate();
     const { pathname } = useLocation();
     //todo think if trick with pathname is the best idea; however we have to take into consideration that from any other component, we wont have access to AppHeader css
+
+    const appHeader = <>
+        <div>
+            <img id="logo" src={logo} className={styles.appLogo} alt="logo" onClick={() => navigate('/')}/>
+        </div>
+    </>
+
+    const userHeader = <>
+        <div className={styles.userHeader} data-testid="AppHeader">
+            <div className={styles.logoPanel}>
+                <img id="logo" src={logo} className={styles.appLogoLeft} alt="logo" onClick={() => navigate('/')}/>
+            </div>
+            <div className={styles.userPanel}>
+                <div>
+                    <label className={styles.text}>Username: ajaskiewicz</label>
+                </div>
+                <div>
+                    <label className={styles.text}>Logout</label>
+                </div>
+            </div>
+        </div>
+    </>
+
     return (
         <div data-testid="AppHeader">
-            <img id="logo" src={logo} className={pathname === '/dashboard' ? styles.AppLogoLeft : styles.AppLogo} alt="logo" onClick={() => navigate('/')}/>
+            {pathname === '/dashboard' ? userHeader : appHeader}
         </div>
     )
 
